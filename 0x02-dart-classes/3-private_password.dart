a96c8dd5f19f356efc63b5a6ef3340808b584af7
Jlaborne <laborne.j@hotmail.fr>
2025-07-16 11:35:41 +0200

    task 3. Private Password

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,23 @@
+class Password {
+  String _password = "";
+
+  Password({String password = ""}) {
+    this._password = password;
+  }
+
+  bool isValid() {
+    if (_password.length >= 8 &&
+        _password.length <= 16 &&
+        RegExp(r'[A-Z]').hasMatch(_password) &&
+        RegExp(r'[a-z]').hasMatch(_password) &&
+        RegExp(r'[0-9]').hasMatch(_password))
+      return true;
+    else
+      return false;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is $_password";
+  }
+}
