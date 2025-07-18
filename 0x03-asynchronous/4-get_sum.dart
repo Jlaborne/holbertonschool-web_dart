d3fbe0e8e4edf335d35a059205e68a1adc66d575
Jlaborne <laborne.j@hotmail.fr>
2025-07-18 12:58:26 +0200

    4. Calcul Total

---
+++ 0x03-asynchronous/4-get_sum.dart
@@ -0,0 +1,24 @@
+import '4-util.dart';
+import 'dart:convert';
+
+Future<double> calculateTotal() async {
+  try {
+    final userData = await fetchUserData();
+    final userMap = jsonDecode(userData);
+    final id = userMap['id'];
+
+    final ordersJson = await fetchUserOrders(id);
+    final productList = jsonDecode(ordersJson);
+
+    double total = 0;
+    for (var product in productList) {
+      final priceJson = await fetchProductPrice(product);
+      final price = jsonDecode(priceJson);
+      total += price;
+    }
+
+    return total;
+  } catch (err) {
+    return -1;
+  }
+}
