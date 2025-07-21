06610f39ceaafe4e4e2788df58c5956f4113d88b
Joshua Laborne <laborne.j@hotmail.fr>
2025-07-21 09:17:40 +0200

    Update task 4

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import '4-util.dart';
 import 'dart:convert';
 
-Future<double> calculateTotal() async {
+calculateTotal() async {
   try {
     final userData = await fetchUserData();
     final userMap = jsonDecode(userData);
